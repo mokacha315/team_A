8d840282871ebc6c99f0d4a1b8f9817b17251777
imaiyuto <h1202515019@hiratagakuen.onmicrosoft.com>
2025-10-20 09:59:57 +0900

    取り急ぎエラー修正

--- team_A/Assets/Imaiyuto/script/playerattack.cs
+++ team_A/Assets/Imaiyuto/script/playerattack.cs
@@ -5,17 +5,18 @@ public class playerAttack : MonoBehaviour
     public GameObject sword;       // Œ•ƒIƒuƒWƒFƒNƒg
     public float attackDelay = 0.2f;  // UŒ‚”»’è‚Ì‘±ŠÔ
     private bool isAttack = false;//UŒ‚’†‚©‚Ç‚¤‚©‚ğ‹L˜^
+
+    public void Attack()
+    {
+        //UŒ‚’†‚Å‚Í‚È‚¢
+        if (isAttack == false)
+        {
+            isAttack = true;//UŒ‚ƒtƒ‰ƒO
+                            //Œ•‚ÅUŒ‚‚·‚é
+
+        }
+    }
+
 }
 
 //UŒ‚
-public void Attack()
-{
-    //UŒ‚’†‚Å‚Í‚È‚¢
-    if(inAttack==false)
-    {
-        inAtaack = true;//UŒ‚ƒtƒ‰ƒO
-        //Œ•‚ÅUŒ‚‚·‚é
-
-
-    }
-}
