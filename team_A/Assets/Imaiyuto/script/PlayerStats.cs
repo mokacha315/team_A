e7677d2fe7b0574c98a6ffdb7721204666f98b4b
imaiyuto <h1202515019@hiratagakuen.onmicrosoft.com>
2025-10-27 09:38:17 +0900

    メイン追加

---
+++ team_A/Assets/Imaiyuto/script/PlayerStats.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class playerStats : MonoBehaviour
+{
+    //Šî‘bUŒ‚—Í
+    public float baseAttackPower = 1f;
+    //ƒoƒt‚É‚æ‚é’Ç‰ÁUŒ‚—Í
+    private float attackbuff = 0f;
+  
+    public float CurrentAttackPower
+    {
+        get { return baseAttackPower; }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
