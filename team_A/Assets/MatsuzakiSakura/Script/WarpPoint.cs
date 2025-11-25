3f4534486b57618a4d28125f3228cafbc9530fbe
matsuzakisakura <h1202515025@hiratagakuen.onmicrosoft.com>
2025-11-25 11:45:51 +0900

    ワープ完成

--- team_A/Assets/MatsuzakiSakura/Script/WarpPoint.cs
+++ team_A/Assets/MatsuzakiSakura/Script/WarpPoint.cs
@@ -2,15 +2,32 @@ using UnityEngine;
 
 public class WarpPoint : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform warpDestination;
+
+    static float warpCooldown = 0.5f;
+
+    static float playerWarpTimer = 0f;
+
+    private void Update()
     {
-        
+        //タイマー減少
+        if (playerWarpTimer > 0)
+        {
+            playerWarpTimer -= Time.deltaTime;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (playerWarpTimer > 0) return;
+
+        //もしPlayerが当たったら
+        if (other.CompareTag("Player"))
+        {
+            //プレイヤーを移動させる
+            other.transform.position = warpDestination.position;
+
+            playerWarpTimer = warpCooldown;
+        }
     }
 }
