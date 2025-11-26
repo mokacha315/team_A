c878055bed496dfeeb25edf05daf81263344b07a
matsuzakisakura <h1202515025@hiratagakuen.onmicrosoft.com>
2025-11-26 10:58:39 +0900

    BGMスクリプト

--- team_A/Assets/MatsuzakiSakura/Script/BGMManager.cs
+++ team_A/Assets/MatsuzakiSakura/Script/BGMManager.cs
@@ -3,13 +3,47 @@ using UnityEngine;
 public class BGMManager : MonoBehaviour
 {
     public AudioSource bgmSource;
-    public
+    public float fadeSpeed = 1.0f;
 
-    AudioSource audioSource;
+    private AudioClip nextClip = null; //次の曲
+    private bool isFading = false;
 
 
-    void Awake()
+    void Update()
     {
-    //    if (instance == null)
+        if (isFading)
+        {
+            //フェードアウト
+            if (bgmSource.volume > 0f && nextClip != null)
+            {
+                bgmSource.volume -= fadeSpeed * Time.deltaTime;
+            }
+            //フェードアウト終わり → 曲切り替え
+            else if (nextClip != null)
+            {
+                bgmSource.clip = nextClip;
+                bgmSource.Play();
+                nextClip = null;
+            }
+            //フェードイン
+            else if (bgmSource.volume < 1f)
+            {
+                bgmSource.volume += fadeSpeed * Time.deltaTime;
+            }
+            else
+            {
+                isFading = false;
+            }
+        }
+    }
+
+
+    public void ChangeBGM(AudioClip newClip)
+    {
+        if (bgmSource.clip == newClip) //同じ曲ならなにもしない
+            return;
+
+        nextClip = newClip;
+        isFading = true;
     }
 }
