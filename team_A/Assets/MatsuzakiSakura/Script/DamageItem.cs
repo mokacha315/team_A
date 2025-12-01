60ab51bcfea7f07d7f044a243d566e14903c5d37
matsuzakisakura <h1202515025@hiratagakuen.onmicrosoft.com>
2025-12-01 12:37:13 +0900

    ダメージバフOK

---
+++ team_A/Assets/MatsuzakiSakura/Script/DamageItem.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class DamageItem : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SwordHit weapon = collision.GetComponent<SwordHit>();
+            if (weapon != null)
+            {
+                weapon.AddDamage(1); // 1å≈íËÇ≈ëùÇ‚Ç∑
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
