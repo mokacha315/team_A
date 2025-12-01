d59ec569bf3679e81cf9ba837564ebb142029787
imaiyuto <h1202515019@hiratagakuen.onmicrosoft.com>
2025-12-01 14:26:22 +0900

    あ

--- team_A/Assets/Imaiyuto/script/SwordHit.cs
+++ team_A/Assets/Imaiyuto/script/SwordHit.cs
@@ -5,7 +5,7 @@ public class SwordHit : MonoBehaviour
     [Header("この武器のダメージ")]
     public int damage = 1;
 
-    private int extraDamage = 1;
+    private int extraDamage = 0;
 
     //現在のダメージ
     public int CurrentDamage => damage + extraDamage;
