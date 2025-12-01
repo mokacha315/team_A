f4ea4f447d18c41de35b8942a8c8211fd8939b52
matsuzakisakura <h1202515025@hiratagakuen.onmicrosoft.com>
2025-12-01 14:46:35 +0900

    攻撃力スクリプト変更

--- team_A/Assets/MatsuzakiSakura/Script/DamageItem.cs
+++ team_A/Assets/MatsuzakiSakura/Script/DamageItem.cs
@@ -6,7 +6,7 @@ public class DamageItem : MonoBehaviour
     {
         if (collision.CompareTag("Player"))
         {
-            SwordHit weapon = collision.GetComponent<SwordHit>();
+            SwordHit weapon = collision.GetComponentInChildren<SwordHit>();
             if (weapon != null)
             {
                 weapon.AddDamage(2); // 1å≈íËÇ≈ëùÇ‚Ç∑
