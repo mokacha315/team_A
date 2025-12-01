098fd6fa0eefa8806f4ff124dc8d0fb82ce25c7d
imaiyuto <h1202515019@hiratagakuen.onmicrosoft.com>
2025-12-01 17:24:24 +0900

    あ

--- team_A/Assets/Imaiyuto/script/SwordHit.cs
+++ team_A/Assets/Imaiyuto/script/SwordHit.cs
@@ -2,9 +2,14 @@ using UnityEngine;
 
 public class SwordHit : MonoBehaviour
 {
-    public int damage = 1;
+    public int damage = 1; // 武器の基本ダメージ
+    public PlayerAttack player; // プレイヤーの参照
+    void Start()
+    {
+        player = FindObjectOfType<PlayerAttack>();
+    }
+    public int CurrentDamage => damage + player.extraDamage;
 }
-
 
 
 
