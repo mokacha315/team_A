bcf92195b91d3e661c194196fd14947de99617d4
matsuzakisakura <h1202515025@hiratagakuen.onmicrosoft.com>
2025-12-02 11:47:10 +0900

    HP

--- team_A/Assets/MatsuzakiSakura/Script/HeroController.cs
+++ team_A/Assets/MatsuzakiSakura/Script/HeroController.cs
@@ -71,6 +71,7 @@ public class HeroController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        ResetStaticVariables();
 
         Application.targetFrameRate = 60;
 
