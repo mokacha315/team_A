06ae223ea5cd7eb8219e4c23d56bab67a7776b68
matsuzakisakura <h1202515025@hiratagakuen.onmicrosoft.com>
2025-12-09 09:27:44 +0900

    リトライ

--- team_A/Assets/MatsuzakiSakura/Script/RetrySEManager.cs
+++ team_A/Assets/MatsuzakiSakura/Script/RetrySEManager.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class RetrySEManager : MonoBehaviour
 {
+    public static RetrySEManager Instance{ get; private set;}
+
     public AudioSource audioSource;
     public AudioClip retryClip;
 
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
     public void PlayRetrySE()
     {
         if (retryClip != null && audioSource != null)
@@ -12,5 +27,4 @@ public class RetrySEManager : MonoBehaviour
             audioSource.PlayOneShot(retryClip);
         }
     }
-
 }
