f47db056fb5c3cc4a540715d1e049587ff7e7bac
imaiyuto <h1202515019@hiratagakuen.onmicrosoft.com>
2026-01-14 12:30:40 +0900

    あ

--- team_A/Assets/Imaiyuto/script/AttackUI.cs
+++ team_A/Assets/Imaiyuto/script/AttackUI.cs
@@ -1,19 +1,35 @@
 using UnityEngine;
 using UnityEngine.UI;
+
 public class AttackUI : MonoBehaviour
 {
-    public SwordHit swordHit;
+    private PlayerAttack player;
     public Text attackText;
 
-
-    void Update()
+    void Start()
     {
-        SwordHit currentSword = FindObjectOfType<SwordHit>();
-
-        if (currentSword != null)
+        // 起動時にシーン内から PlayerAttack を自動で探して記憶する
+        player = FindObjectOfType<PlayerAttack>();
+    }
+        void Update()
         {
-            attackText.text = "ATK:" + currentSword.CurrentDamage;
+            if (player != null && attackText != null)
+            {
+                int baseAtk = 0;
+
+                // 武器データが存在し、かつ参照が生きているかチェック
+                if (player.currentWeapon != null)
+                {
+                    baseAtk = player.attackPower;
+                }
+                else
+                {
+                    // 武器がない場合や初期化前の暫定値
+                    baseAtk = 1;
+                }
+
+                int totalAtk = baseAtk + player.extraDamage;
+                attackText.text = "ATK:" + totalAtk;
+            }
         }
     }
-    }
-
--- team_A/Assets/Imaiyuto/script/AttackUIAutoSetter.cs
+++ team_A/Assets/Imaiyuto/script/AttackUIAutoSetter.cs
@@ -1,13 +1,13 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class AttackUIAutoSetter : MonoBehaviour
-{
-    void Awake()
-    {
-        AttackUI ui = GetComponent<AttackUI>();
-        if (ui != null && ui.swordHit == null)
-        {
-            ui.swordHit = FindObjectOfType<SwordHit>();
-        }
-    }
-}
+//public class AttackUIAutoSetter : MonoBehaviour
+//{
+//    void Awake()
+//    {
+//        AttackUI ui = GetComponent<AttackUI>();
+//        if (ui != null && ui.swordHit == null)
+//        {
+//            ui.swordHit = FindObjectOfType<SwordHit>();
+//        }
+//    }
+//}
